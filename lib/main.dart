7c6ed2b774696931a0017e5c8b8f11fc2225f914
JanaviMahadik <janavimahadik06@gmail.com>
2024-02-23 11:56:45 +0530

    change the title to Task Manager App

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Auto Navigate Example',
+        title: 'Task Manager App',
         initialRoute: '/logo',
         routes: {
           '/': (context) => TodoScreen(),
